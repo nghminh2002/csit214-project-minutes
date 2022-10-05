7aada68201308860d06092193647b4e98492f5a4
Milly Nguyen <minh.hue@savvycomsoftware.com>
2022-10-05 20:20:49 +0700

    CSIT214-49 create homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,37 @@
 import type { NextPage } from "next";
 import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import Header from "../layouts/Header";
-import Footer from "../layouts/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import SearchFlightBar from "../components/SearchFlightBar";
+import EveryThingInOnePlace from "../layout/homepage/EveryThingInOnePlace";
+import ExploreExperience from "../layout/homepage/ExploreExperience";
+import ExploreVacation from "../layout/homepage/ExploreVacation";
+import TitleBanner from "../layout/homepage/TitleBanner";
 
 const Banner = styled(Box)({
-  backgroundImage: "url(/Background1.svg)",
+  backgroundImage:
+    "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(/Background1.svg)",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backdropFilter: "blur(2px)",
+  height: "700px",
   position: "relative",
-  height: "50vh",
 });
 
-const Home: NextPage = () => {
-  return (
-    <Box>
-      <Header />
-      <Banner />
-      <Footer />
-    </Box>
-  );
-};
+const Home: NextPage = () => (
+  <Box>
+    <Header />
+    <Banner>
+      <TitleBanner />
+    </Banner>
+    <SearchFlightBar />
+    <ExploreVacation />
+    <ExploreExperience />
+    <EveryThingInOnePlace />
+    <Footer />
+  </Box>
+);
 
 export default Home;
